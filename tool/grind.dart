540321c10b5a0b9d5d88245ae855433c42606a6f
andikadeveloper <mandikadev@gmail.com>
2024-09-21 15:15:33 +0700

    update dart release to use grind

---
+++ tool/grind.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+import 'package:cli_pkg/cli_pkg.dart' as pkg;
+import 'package:grinder/grinder.dart';
+
+const _packageName = 'dart_github_workflow';
+const owner = 'andikadeveloper';
+const repo = 'dart-github-workflow';
+
+void main(List<String> args) {
+  pkg.name.value = _packageName;
+  pkg.humanName.value = _packageName;
+  pkg.githubUser.value = owner;
+  pkg.githubRepo.value = '$owner/$_packageName';
+  pkg.githubBearerToken.value = Platform.environment['GITHUB_TOKEN'];
+
+  pkg.addAllTasks();
+
+  grind(args);
+}
+
+@Task('Compile')
+void compile() {
+  run('dart', arguments: ['compile', 'exe', 'bin/main.dart', '-o', 'fvm']);
+}
